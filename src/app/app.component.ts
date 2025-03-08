83b1260af5f14cdbbfb7f675e2d09062788c7ddf
Leandro K <leandrokesseler@gmail.com>
2025-03-08 17:32:03 -0300

    Inicio
    Used cards and carrousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./menu/menu.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
