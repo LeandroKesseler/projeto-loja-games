76e172d862617ecb929169e9bcc68d1b3faa5619
Leandro K <leandrokesseler@gmail.com>
2025-03-08 23:17:35 -0300

    Login and Footer
    Two news itens added to the page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./menu/menu.component";
 import { InicioComponent } from "./inicio/inicio.component";
+import { LoginComponent } from "./login/login.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, InicioComponent],
+  imports: [RouterOutlet, MenuComponent, InicioComponent, LoginComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
