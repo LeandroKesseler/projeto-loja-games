3906361ad8c39dff9b7c6dccb8b1463453d8482d
Leandro K <leandrokesseler@gmail.com>
2025-03-09 00:34:09 -0300

    Using Routes
    RouterOutlet and RouterLinks

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
